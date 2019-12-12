11ea1987ed1bc43975fce4bd95d0162f464a47ba
Morten Lindhart <morten@lindhart.com>
2019-12-12 21:03:34 +0100

    missing for task 8 part 1

---
+++ Task8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskA();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void TaskA()
+        {
+            Image img = new Image(6, 25);
+            img.Parse(Parse(Input.InputTask1));
+
+            ImageLayer fewestZeroes = new ImageLayer(img);
+            int leastZero = int.MaxValue;
+
+            foreach (var imageLayer in img.Layers)
+            {
+                int zeroes = imageLayer.Lines.Sum(x => x.Count(y => y == 0));
+                if (zeroes < leastZero)
+                {
+                    leastZero = zeroes;
+                    fewestZeroes = imageLayer;
+                }
+            }
+
+            var flat = fewestZeroes.Lines.SelectMany(x => x).ToArray();
+            var result = flat.Count(x => x == 1) * flat.Count(y => y == 2);
+
+            Console.WriteLine($"Result of 8.1: " + result);
+        }
+
+        private static void TestIt()
+        {
+            Image img = new Image(2, 3);
+            img.Parse(Parse(Input.TestInput));
+
+        }
+
+        public static List<int> Parse(string input)
+        {
+            return new List<int>(input.Select(x => int.Parse(x.ToString())));
+        }
+    }
+}
